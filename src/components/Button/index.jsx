de7b1415cf12046a82ee9f0acf84c0b451d85972
Matheus Ferreira <matheus.fer33@gmail.com>
2022-09-25 16:04:07 -0300

    initial commit

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+export const Button = ({ text, onClick, disabled = false }) => (
+  <button className="button" onClick={onClick} disabled={disabled}>
+    {text}
+  </button>
+);
---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+export const TextInput = ({ searchValue, handleChange }) => (
+  <input
+    className="text-input"
+    onChange={handleChange}
+    value={searchValue}
+    type="search"
+    placeholder="Type your search"
+  />
+);
